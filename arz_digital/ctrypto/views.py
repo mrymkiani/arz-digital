c7bc8a1356704a4036e229ac00b3f8e617ebec0a
aria <aria.legion2@gmail.com>
2024-09-12 11:55:18 +0330

    edited views

--- arz_digital/ctrypto/views.py
+++ arz_digital/ctrypto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView 
-from .models import Crypto
+from .models import Crypto ,  Alert
 from .serializer import CryptoSerializer
+
 
 # Create your views here.
