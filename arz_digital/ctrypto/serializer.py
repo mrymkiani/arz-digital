3b14803a26f4d5f9b14f885adc80ac9c930a6de1
aria <aria.legion2@gmail.com>
2024-09-12 12:36:57 +0330

    alert serializer

--- arz_digital/ctrypto/serializer.py
+++ arz_digital/ctrypto/serializer.py
@@ -1,7 +1,12 @@
-from .models import Crypto
+from .models import Crypto , Alert
 from rest_framework.serializers import ModelSerializer
 
 class CryptoSerializer(ModelSerializer):
     class Meta:
         model = Crypto
+        fields = '__all__'
+
+class AlertSerializer(ModelSerializer):
+    class Meta:
+        model = Alert
         fields = '__all__'
