11cb5aedeab47f4bcf8bf58a72a7399ee9077c8c
Your Name <you@example.com>
2024-09-12 02:08:16 -0700

    view

--- arz_digital/ctrypto/views.py
+++ arz_digital/ctrypto/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse, JsonResponse, HttpResponseBadRequest
 from rest_framework.generics import ListAPIView 
-from .models import Crypto
+from .models import Crypto , Alert
 from .serializer import CryptoSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.core.mail import send_mail
+from django.conf import settings
+import requests
+import json
+
+
+
+def get(request ,srccurrency ):
+    url = 'https://api.nobitex.ir/v2/market/stats?srcCurrency={}&dstCurrency={}'.format('srccurrency', "rls")
+    response = requests.get(url)
+    res = {
+            "price":json.loads(response.content) . get("Latest") , 
+            "price_chane_24":json.loads(response.content).get("dayChange")
+        }
+    cryptos = Crypto.objects.create(
+        crypto =srccurrency , 
+        price = res['price'] , 
+        price_chane_24 = res['price_chane_24'])
+    return HttpResponse (cryptos)
+    
+
+
+class AlertViewSet(viewsets.ModelViewSet):
+    queryset = Alert.objects.all()
+    serializer_class = AlertSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        alert = serializer.save()
+        check_alerts()
+
+def check_alerts():
+    alerts = Alert.objects.all()
+    for alert in alerts:
+        if alert.cryptocurrency.price >= alert.target_price:
+            send_mail(
+                'Crypto Alert!',
+                f'The price of {alert.cryptocurrency.name} has reached {alert.target_price}.',
+                settings.DEFAULT_FROM_EMAIL,
+                [alert.user.email],
+                fail_silently=False,
+            )
 
 # Create your views here.
